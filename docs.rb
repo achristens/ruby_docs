a69fba22fafd88d61e5c2960f0098adf1319ca02
Abby Christens <achristens@gmail.com>
2017-09-22 13:47:22 -0400

    Defining array methods

--- docs.rb
+++ docs.rb
@@ -36,15 +36,43 @@ irb(main):029:0> "Bitmaker is teaching us Ruby".start_with?("bit")
 => false
 
 
-
-
 # Array
-#
-# first
-# delete_at
-# delete
-# pop
-#
+
+# first returnst the first element (or elements). Specifying an (n) gives you the first n number of elements in the array
+irb(main):002:0 a = [1, 2, 3, 4, 5]
+=> [1, 2, 3, 4, 5]
+irb(main):003:0> a.first
+=> 1
+irb(main):004:0> a.first(3)
+=> [1, 2, 3]
+
+# delete_at deletes an array element at a specific index. Requires one argument, the index number
+irb(main):009:0> pets = ['dog', 'cat', 'fish', 'rabbit']
+=> ["dog", "cat", "fish", "rabbit"]
+irb(main):010:0> pets.delete_at(3)
+=> "rabbit"
+irb(main):011:0> pets
+=> ["dog", "cat", "fish"]
+
+# delete deletes a specific array element, regardless of index. Requires one argument, the content of the array item.
+irb(main):012:0> greetings = ["hi", "Hello", "whats up"]
+=> ["hi", "Hello", "whats up"]
+irb(main):013:0> greetings.delete("hi")
+=> "hi"
+irb(main):014:0> greetings
+=> ["Hello", "whats up"]
+
+# pop removes the last element from the array. OPtional arguemnt to provide a number of items to remove (always from the end).
+irb(main):020:0> array = ["one", "two", "red", "blue"]
+=> ["one", "two", "red", "blue"]
+irb(main):021:0> array.pop
+=> "blue"
+irb(main):022:0> array.pop(2)
+=> ["two", "red"]
+irb(main):023:0> array
+=> ["one"]
+
+
 # Hash
 #
 # to_a
